906498eda75f74f7372de660cf76bacf1e2ba177
scarfacedotcom <peteradaaku@gmail.com>
2023-03-03 05:04:27 +0100

    added the two contracts files

---
+++ contracts/ScarFaceToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TimidanToken is ERC20("ScarFaceToken", "SFT") {
+    address public owner;
+    uint8 public _decimals;
+    uint256 public _totalSupply;
+
+    constructor() {
+        _decimals = 18;
+        _totalSupply = 10000000 * 10 ** _decimals;
+        owner = msg.sender;
+        _mint(address(this), _totalSupply);
+    }
+
+    modifier onlyOwner() {
+        msg.sender == owner;
+        _;
+    }
+
+    function transferOut(address addressTo, uint amountTo) external onlyOwner {
+        uint balContract = balanceOf(address(this));
+        require(balContract >= amountTo, "not enough");
+        _transfer(address(this), addressTo, amountTo);
+    }
+
+    function mint(uint amount) internal {
+        _mint(msg.sender, amount);
+    }
+}
